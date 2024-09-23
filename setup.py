5094abaab9431e017feff262161b073c032e0bc3
Morteza Khazaei <mortezakhazaei1370@gmail.com>
2024-09-23 15:32:27 -0600

    update setup and tests

--- setup.py
+++ setup.py
@@ -1,26 +1,36 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+def read_requirements(file):
+    with open(file) as f:
+        return f.read().splitlines()
 
-setuptools.setup(
-    name="RISMA",
-    version="0.0.1",
-    author="Morteza Khazaei",
-    author_email="khazaei.morteza@ut.ac.ir",
-    description="Agriculture and Agri-Food Canada (AAFC)",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Morteza-Khazaei/RISMA",
-    project_urls={
-        "Bug Tracker": "https://github.com/Morteza-Khazaei/RISMA/issues",
-    },
+def read_file(file):
+   with open(file) as f:
+        return f.read()
+    
+long_description = read_file('README.md')
+version = read_file('VERSION')
+requirements = read_requirements('requirements.txt')
+
+setup(
+    name = 'RISMA',
+    version = version,
+    author = 'Morteza Khazaei',
+    author_email = 'morteza.khazaei@usherbrooke.ca',
+    url = 'https://github.com/Morteza-Khazaei/RISMA',
+    description = 'Agriculture and Agri-Food Canada (AAFC)',
+    long_description_content_type = 'text/x-rst',  # If this causes a warning, upgrade your setuptools package
+    long_description = long_description,
+    license = 'MIT license',
+    package_dir={'': 'src'},
+    packages = find_packages(
+        where='src', 
+        exclude=['dataset']
+    ),  # Don't include test directory in binary distribution
+    install_requires = requirements,
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]  # Update these accordingly
 )
