eb4d35285dd0080d30d30ca4e4fede77b4b75e3b
Morteza Khazaei <mortezakhazaei1370@gmail.com>
2025-07-07 16:00:22 -0600

    new setup file

--- setup.py
+++ setup.py
@@ -1,36 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-def read_requirements(file):
-    with open(file) as f:
-        return f.read().splitlines()
-
-def read_file(file):
-   with open(file) as f:
-        return f.read()
-    
-long_description = read_file('README.md')
-version = read_file('VERSION')
-requirements = read_requirements('requirements.txt')
-
-setup(
-    name = 'RISMA',
-    version = version,
-    author = 'Morteza Khazaei',
-    author_email = 'morteza.khazaei@usherbrooke.ca',
-    url = 'https://github.com/Morteza-Khazaei/RISMA',
-    description = 'Agriculture and Agri-Food Canada (AAFC)',
-    long_description_content_type = 'text/x-rst',  # If this causes a warning, upgrade your setuptools package
-    long_description = long_description,
-    license = 'MIT license',
-    package_dir={'': 'src'},
-    packages = find_packages(
-        where='src', 
-        exclude=['dataset']
-    ),  # Don't include test directory in binary distribution
-    install_requires = requirements,
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ]  # Update these accordingly
-)
+if __name__ == "__main__":
+    setup()
